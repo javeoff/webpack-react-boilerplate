d9e6efbdf0892985505ee31c3bd0c7f0c2c32886
Даниил Бондарев <d.bondarev@maximaster.ru>
2021-06-01 21:33:23 +0300

    + ForkTsChecker + HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,17 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const path = require('path');
+
 module.exports = {
+  context: __dirname,
   mode: 'development',
   watch: true,
   entry: './src/index.tsx',
   output: {
     filename: 'bundle.js',
-    path: `${__dirname}/dist`,
+    path: path.resolve(__dirname, './dist'),
   },
+  plugins: [new HtmlWebpackPlugin(), new ForkTsCheckerWebpackPlugin()],
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.json'],
   },
@@ -13,7 +19,6 @@ module.exports = {
   module: {
     rules: [
       { test: /\.scss$/, use: ['style-loader', 'css-loader'] },
-      { test: /\.tsx?$/, loader: 'babel-loader' },
       { test: /\.tsx?$/, loader: 'ts-loader' },
       { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' },
     ],
